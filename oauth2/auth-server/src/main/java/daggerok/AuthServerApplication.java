9b0793b9c922385cc2304fa0311649dc59591771
Maksim Kostromin <daggerok@gmail.com>
2017-05-26 10:52:30 +0300

    implemented jwt using symmetric sugning key

--- oauth2/auth-server/src/main/java/daggerok/AuthServerApplication.java
+++ oauth2/auth-server/src/main/java/daggerok/AuthServerApplication.java
@@ -6,155 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.TimeZone;
 
 /**
- *
- * 1. password:
- *
- * post /oauth/token?grant_type=password&username=$username&password=$password
- *
- * ie:
- *
- * http --auth admin-app:admin post :9999/uaa/oauth/token\?grant_type=password\&username=user\&password=pass
+ * http -pb --auth admin-app:admin post :9999/uaa/oauth/token\?grant_type=password\&username=user\&password=pass   10:58:25
  * {
- *  "access_token": "ad269b9d-e366-4107-a3a3-a0e9af74e214",
+ *  "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJ1c2VyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm9sb2xvIjoidHJvbG9sbyIsImV4cCI6MTQ5NTgzMDUzMiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BQ1RVQVRPUiJdLCJqdGkiOiI4NDI0MTJjZS00YzJiLTQ5MzUtOTJlZC1jZGJlM2FjMmIzZjkiLCJjbGllbnRfaWQiOiJhZG1pbi1hcHAifQ.7QjvPRZHSZgPDUNHTkDQT8nggR_CYs4RSI9lwPpSFmw",
  *  "expires_in": 43199,
- *  "refresh_token": "5e5106da-7afb-4f4f-a577-49e295f51557",
+ *  "jti": "842412ce-4c2b-4935-92ed-cdbe3ac2b3f9",
+ *  "ololo": "trololo",
+ *  "refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJ1c2VyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sImF0aSI6Ijg0MjQxMmNlLTRjMmItNDkzNS05MmVkLWNkYmUzYWMyYjNmOSIsIm9sb2xvIjoidHJvbG9sbyIsImV4cCI6MTQ5ODM3OTMzMiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BQ1RVQVRPUiJdLCJqdGkiOiI3YTVmOWFjZi04NDVjLTQ5NmYtYjlhYi1lZTg3NmY1NDE1ZTIiLCJjbGllbnRfaWQiOiJhZG1pbi1hcHAifQ.d20NFm_pRnC0fm3E6ZSVUjMHJmtiQSEgA3TcCJk5pGA",
  *  "scope": "read write",
  *  "token_type": "bearer"
  * }
- *
- *
- * 2. check token:
- *
- * post /oauth/check_token?token=$access_token
- *
- * ie:
- *
- * http --auth admin-app:admin post :9999/uaa/oauth/check_token\?token=ad269b9d-e366-4107-a3a3-a0e9af74e214
- *
- * {
- *  "authorities": [
- *    "ROLE_USER",
- *    "ROLE_ACTUATOR"
- *  ],
- *  "client_id": "admin-app",
- *  "exp": 1495807797,
- *  "scope": [
- *    "read",
- *    "write"
- *  ],
- *  "user_name": "user"
- * }
- *
- *
- * 3. refresh token:
- *
- * post /oauth/token?grant_type=refresh_token&refresh_token=$refreshToken
- *
- * ie:
- *
- * http --auth admin-app:admin post :9999/uaa/oauth/token\?grant_type=refresh_token\&refresh_token=5e5106da-7afb-4f4f-a577-49e295f51557
- *
- * {
- *  "access_token": "ef7d67b8-add1-4fa8-b2d3-ede7a72c7a5d",
- *  "expires_in": 43199,
- *  "refresh_token": "5e5106da-7afb-4f4f-a577-49e295f51557",
- *  "scope": "read write",
- *  "token_type": "bearer"
- * }
- *
- *
- * 4. client credentials:
- *
- * post /oauth/token?grant_type=client_credentials
- *
- * ie:
- *
- * http --auth admin-app:admin post :9999/uaa/oauth/token\?grant_type=client_credentials
- *
- * {
- *  "access_token": "5d356e7a-187b-4fc2-a559-3ce6aee5e4e1",
- *  "expires_in": 43199,
- *  "scope": "read write",
- *  "token_type": "bearer"
- * }
- *
- *
- * 5. code (use browser):
- *
- * browser http://$authServerHost/oauth/authorize?response_type=code&client_id=$clientId&redirect_uri=$redirectUrl
- *
- * ie:
- *
- * firefox http://localhost:9999/uaa/oauth/authorize?response_type=code&client_id=admin-app&redirect_uri=http://example.com
- *
- * choose scope read
- * click approve
- *
- * it will redirect you to:
- * http://example.com/?code=1fYSVu
- *
- * then application on example.com should prepare request:
- *
- * post /oauth/token?grant_type=authorization_code&code=$code&redirect_uri=$redirectUri&client_id=$clientId
- *
- * ie:
- *
- * http --auth admin-app:admin post :9999/uaa/oauth/token\?grant_type=authorization_code\&code=1fYSVu\&redirect_uri=http://example.com\&client_id=admin-app
- *
- * {
- *  "access_token": "93b4ba2a-384f-47d6-9093-32bda8ff033c",
- *  "expires_in": 43199,
- *  "refresh_token": "6cd61fac-4701-4b5b-9a2e-320b72102bf3",
- *  "scope": "read",
- *  "token_type": "bearer"
- * }
- *
- *
- * 6. implicit:
- *
- * browser http://$authServerHost/oauth/authorize?response_type=token&client_id=$clientId&state=$optionalState&redirect_uri=$redirectUri
- *
- * ie:
- *
- * firefox http://localhost:9999/uaa/oauth/authorize?response_type=token&client_id=admin-app&state=xyz&redirect_uri=http://example.com
- *
- * choose read scope
- * click approve
- *
- * you will be redirected to:
- * http://example.com/#access_token=bc423e9c-b145-4c66-b08c-a829c8251644&token_type=bearer&state=xyz&expires_in=43199&scope=read
- *
- * ie:
- *
- * {
- *   "access_token": "bc423e9c-b145-4c66-b08c-a829c8251644"
- *   "token_type": "bearer"
- *   "state": "xyz"
- *   "expires_in": "43199"
- *   "scope": "read"
- * }
- *
- *
- * 7. password
- *
- * post /oauth/token?grant_type=password&username=$username&password=$password
- *
- * ie:
- *
- * http --auth admin-app:admin post :9999/uaa/oauth/token\?grant_type=password\&username=user\&password=pass
- *
- * {
- *  "access_token": "91b88c40-bb73-4a3d-b7dd-f565211a6f47",
- *  "expires_in": 43199,
- *  "refresh_token": "b5b27853-aeb1-4edb-96f1-1da00e9d5adc",
- *  "scope": "read write",
- *  "token_type": "bearer"
- * }
- *
- *
- * read more:
- * https://tools.ietf.org/html/rfc6749#section-4
- *
  */
 @SpringBootApplication
 public class AuthServerApplication {
