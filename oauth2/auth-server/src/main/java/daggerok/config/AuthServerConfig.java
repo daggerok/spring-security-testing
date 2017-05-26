3449555cfee1cd80fd6ee176e8c7ce69978f2e4d
Maksim Kostromin <daggerok@gmail.com>
2017-05-26 10:52:30 +0300

    implemented using asymmetric keystores with jks on auth-server and as public key string value on resource-server

--- oauth2/auth-server/src/main/java/daggerok/config/AuthServerConfig.java
+++ oauth2/auth-server/src/main/java/daggerok/config/AuthServerConfig.java
@@ -5,7 +5,9 @@ import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -14,15 +16,17 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.*;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonMap;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
-  final JwtTokenEnhancer jwtTokenEnhancer;
+  final JwtProperties jwtProps;
   final AuthenticationManager authenticationManager;
 
   @Bean
@@ -38,7 +42,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
   @Bean
   public JwtAccessTokenConverter jwtAccessTokenConverter() {
     val jwtAccessTokenConverter = new JwtAccessTokenConverter();
-    jwtAccessTokenConverter.setSigningKey("123");
+    val resource = new ClassPathResource(jwtProps.filePath);
+    val keyStoreKeyFactory = new KeyStoreKeyFactory(resource, jwtProps.password.toCharArray());
+    val keyPair = keyStoreKeyFactory.getKeyPair(jwtProps.alias);
+    jwtAccessTokenConverter.setKeyPair(keyPair);
     return jwtAccessTokenConverter;
   }
 
@@ -52,42 +59,54 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
   }
 
   @Bean
+  TokenEnhancer jwtTokenEnhancer() {
+    return (accessToken, authentication) -> {
+      val defaultOAuth2AccessToken = (DefaultOAuth2AccessToken) accessToken;
+      defaultOAuth2AccessToken.setAdditionalInformation(singletonMap("ololo", "trololo"));
+      return accessToken;
+    };
+  }
+
+  @Bean
   TokenEnhancerChain tokenEnhancerChain() {
     val tokenEnhancerChain = new TokenEnhancerChain();
-    tokenEnhancerChain.setTokenEnhancers(asList(jwtTokenEnhancer, jwtAccessTokenConverter()));
+    tokenEnhancerChain.setTokenEnhancers(asList(jwtTokenEnhancer(), jwtAccessTokenConverter()));
     return tokenEnhancerChain;
   }
 
   @Override
   public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
-
+    // @formatter:off
     clients
-        .inMemory()
+      .inMemory()
         .withClient("public-app")
-        .secret("client")
-        .authorizedGrantTypes("implicit", "password", "client_credentials", "authorization_code", "refresh_token")
-        .scopes("read")
-        .and()
+          .secret("client")
+          .authorizedGrantTypes("implicit", "password", "client_credentials", "authorization_code", "refresh_token")
+          .scopes("read")
+      .and()
         .withClient("admin-app")
-        .secret("admin")
-        .authorizedGrantTypes("implicit", "password", "client_credentials", "authorization_code", "refresh_token")
-        .scopes("read", "write");
+          .secret("admin")
+          .authorizedGrantTypes("implicit", "password", "client_credentials", "authorization_code", "refresh_token")
+          .scopes("read", "write");
+    // @formatter:on
   }
 
   @Override
   public void configure(final AuthorizationServerSecurityConfigurer security) throws Exception {
-
+    // @formatter:off
     security
-        .tokenKeyAccess("permitAll()")
-        .checkTokenAccess("isAuthenticated()");
+      .tokenKeyAccess("permitAll()")
+      .checkTokenAccess("isAuthenticated()");
+    // @formatter:on
   }
 
   @Override
   public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-
+    // @formatter:off
     endpoints
-        .tokenStore(tokenStore())
-        .tokenEnhancer(tokenEnhancerChain())
-        .authenticationManager(authenticationManager);
+      .tokenStore(tokenStore())
+      .tokenEnhancer(tokenEnhancerChain())
+      .authenticationManager(authenticationManager);
+    // @formatter:on
   }
 }
--- oauth2/resource-server/src/main/java/daggerok/ResourceServerApplication.java
+++ oauth2/resource-server/src/main/java/daggerok/ResourceServerApplication.java
@@ -6,28 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.TimeZone;
 
 /**
- * 1:
- * http --auth admin-app:admin post :9999/uaa/oauth/token\?grant_type=password\&username=user\&password=pass       08:40:34
- * {
- *  "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJ1c2VyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm9sb2xvIjoidHJvbG9sbyIsImV4cCI6MTQ5NTgyMDQ5MywiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BQ1RVQVRPUiJdLCJqdGkiOiIxNmQxNDEwZS02ODM0LTRjNTEtOGJjNS1lYWEzMjlmNzkzZjMiLCJjbGllbnRfaWQiOiJhZG1pbi1hcHAifQ.5H2JeKzQA24j9i-9CL5jfiy2tbduEtv3g3XqmpLjG0M",
- *  "expires_in": 43199,
- *  "jti": "16d1410e-6834-4c51-8bc5-eaa329f793f3",
- *  "ololo": "trololo",
- *  "refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJ1c2VyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sImF0aSI6IjE2ZDE0MTBlLTY4MzQtNGM1MS04YmM1LWVhYTMyOWY3OTNmMyIsIm9sb2xvIjoidHJvbG9sbyIsImV4cCI6MTQ5ODM2OTI5MywiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BQ1RVQVRPUiJdLCJqdGkiOiI4NTdhMmRlNC02ZWQ4LTRkNmMtOTM5Mi0yNmEwZTA2ODAwZjMiLCJjbGllbnRfaWQiOiJhZG1pbi1hcHAifQ.gaO1Cum5aeomt-KJtrPwAnCuXzJr-5ZDW7NB5lbizx4",
- *  "scope": "read write",
- *  "token_type": "bearer"
- * }
+ * export token=ODcxMywiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BQ1RVQVRPUiJdLCJqdGkiOiJmOTFkY2IzOS01ZmZjLTRmNjAtOWFlMC05NDE1NTE5NTM3MzYiLCJjbGllbnRfaWQiOiJhZG1pbi1hcHAifQ.ZsNqblvPd3I3I5z7dvH4EU0Jx17E6kaEAuamhC5I4yUcyw5CNK-HO_Tc7a1N8ABSZkt3hQ_C9H4pl7JTuvqUbsd_WIpv-sZKhET4MbthGpvI7BzXbQmdkHMvrwS7xoYABMOLlIshNivSJxQQpsTYv4dxPRUoQSVStNJGJacrmRLSvaczIHITRDpMPEpTd9Ga6LNSf1H-yCS0Wdjf5ExlDrqQaVC83_pnB8iJJKCPFAxDtO3uNgf1lAIwtMzQd3adbIuhMZ_6xc8OydxbTVuNKYvT-8f3kWKxM7K2q28fqmBuluf4ZsuZNtsmwKEHy0w8IfCu56VgR2hyGw2NTWppAA
  *
- * 2:
- * export token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJ1c2VyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm9sb2xvIjoidHJvbG9sbyIsImV4cCI6MTQ5NTgyMDQ5MywiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIiwiUk9MRV9BQ1RVQVRPUiJdLCJqdGkiOiIxNmQxNDEwZS02ODM0LTRjNTEtOGJjNS1lYWEzMjlmNzkzZjMiLCJjbGllbnRfaWQiOiJhZG1pbi1hcHAifQ.5H2JeKzQA24j9i-9CL5jfiy2tbduEtv3g3XqmpLjG0M
- *
- * 3:
- * http -pb post :8080/ username=test "Authorization: Bearer $token"
- * http -pb :8080/ "Authorization: Bearer $token"
+ * http -pb :8080/ "Authorization: Bearer $token"                                                                  10:59:45
  * [
  *  {
  *    "id": 1,
  *    "username": "test"
+ *  },
+ *  {
+ *    "id": 2,
+ *    "username": "test1"
  *  }
  * ]
  */
